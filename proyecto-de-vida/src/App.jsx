00276c30e3999653ae73de1a3130ecf7afa6711b
Juan Alvarez <alvarez.juanign@gmail.com>
2023-07-03 17:00:35 -0300

    Add animations

--- proyecto-de-vida/src/App.jsx
+++ proyecto-de-vida/src/App.jsx
@@ -1,5 +1,4 @@
 import church from './assets/church.svg'
-import gallery from './assets/gallery.svg'
 import phone from './assets/chat.svg'
 import clock from './assets/clock.svg'
 import logo from './assets/logo.png'
@@ -12,17 +11,11 @@ export function App () {
       <main className='h-screen' id='home'>
         <header>
           <nav className='flex flex-col items-center'>
-            <ul className='w-[90%] mt-6 flex justify-evenly items-center h-16 bg-gray-500/75 gap-8 shadow-lg rounded-md'>
+            <ul className='w-[95%] mt-2 flex justify-evenly items-center h-16 bg-gray-500/70 gap-8 shadow-lg rounded-md'>
               <li>
                 <a href='#home' className='flex flex-col justify-center items-center'>
                   <img src={church} className='h-6' />
                   <p className='font-extrabold'>Inicio</p>
-                </a>
-              </li>
-              <li>
-                <a href='#gallery' className='flex flex-col justify-center items-center'>
-                  <img src={gallery} className='h-6' />
-                  <p className='font-extrabold'>Galería</p>
                 </a>
               </li>
               <li>
@@ -35,9 +28,9 @@ export function App () {
           </nav>
         </header>
 
-        <section className='h-screen flex flex-col items-center gap-5 mt-16'>
+        <section className='h-screen flex flex-col items-center gap-5 mt-4 '>
           <img src={logo} alt='logo de la iglesia' className='drop-shadow-lg h-80' />
-          <div className='m-1'>
+          <div className='m-1 animate-fade-right animate-once animate-duration-1000 animate-ease-in-out'>
             <h3 className='font-semibold text-xl text-center underline m-2 cursor-default'>Virtual</h3>
             <div>
               <a href='https://www.facebook.com/jose.zavaleta.169405' className='flex justify-center items-center gap-6 bg-[#77C6CD]/60 rounded-md shadow-lg h-32 p-3'>
@@ -55,7 +48,7 @@ export function App () {
             </div>
           </div>
 
-          <div className='m-1'>
+          <div className='animate-fade-left animate-once animate-duration-1000 animate-ease-in-out'>
             <h3 className='font-semibold text-xl text-center underline m-2 cursor-default'>Presencial</h3>
             <div className='flex flex-col items-center bg-[#77C6CD]/60 rounded-md shadow-lg h-32 p-3 cursor-default'>
               <img src={clock} alt='clock' className='h-14' />
@@ -69,27 +62,12 @@ export function App () {
         </section>
       </main>
 
-      <section className='my-7 mx-[10%]' id='gallery'>
-        <div className='grid gap-7 grid-cols-2 justify-stretch place-items-stretch'>
-          <div className='bg-white h-48' />
-          <div className='bg-red-300 h-48' />
-          <div className='bg-red-700 h-48' />
-          <div className='bg-amber-300 h-48' />
-          <div className='bg-lime-500 h-48' />
-          <div className='bg-teal-500 h-48' />
-          <div className='bg-indigo-700 h-48' />
-          <div className='bg-pink-400 h-48' />
-          <div className='bg-blue-800 h-48' />
-          <div className='bg-orange-600 h-48' />
-        </div>
-      </section>
-
       <footer id='contact' className='bg-gray-500/75 h-28 flex flex-col items-center justify-center gap-3'>
         <div className='flex justify-center gap-4'>
           <a href='https://www.facebook.com/iglesiadedios.florestanorte'><img src={facebookLogo} alt='ícono de facebook' className='h-6' /></a>
           <a href='https://goo.gl/maps/YZrSezH9TQe6Jyy76'><img src={locationLogo} alt='ícono de ubicación' className='h-6' /></a>
         </div>
-        <p className='text-center font-semibold'>©️ Todos los derechos reservados.</p>
+        <p className='text-center font-semibold'>©️ 2023. Proyecto de Vida todos los derechos reservados.</p>
       </footer>
     </body>
   )
