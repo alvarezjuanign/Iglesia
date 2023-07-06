1e948ac7bf7f05183e2e8f1d23f42432748f179d
Juan Alvarez <alvarez.juanign@gmail.com>
2023-07-06 17:43:18 -0300

    Initial commit

--- pagina-web/src/components/Card.jsx
+++ pagina-web/src/components/Card.jsx
@@ -1,17 +1,13 @@
-import clock from './assets/clock.svg'
-import logo from './assets/logo.png'
-import facebookLogo from './assets/facebook.svg'
-import locationLogo from './assets/location.svg'
-import encuentrosImg from './assets/encuentros.jpg'
+import clockSvg from '../../public/clock.svg'
 
-export function App () {
+export function Home () {
   return (
-    <body>
-      <main className='bg-[#edebeb] flex flex-col gap-24 items-center'>
+    <main className='w-screen'>
+      <div className='bg-[#edebeb] flex flex-col gap-24 items-center'>
         <header className='h-1/2 flex flex-col items-center gap-4 pt-14'>
           <picture>
             <img
-              src={logo}
+              src='../../public/logo.webp'
               alt='logo de la iglesia'
               className='drop-shadow-lg h-32'
             />
@@ -33,24 +29,24 @@ export function App () {
         <section className='flex flex-col items-center my-11'>
           <picture>
             <img
-              src={encuentrosImg}
+              src='../../public/encuentros.webp'
               alt='foto de los encuentros en la iglesia'
-              className='h-72 rounded-md'
+              className='h-60 rounded-md'
             />
           </picture>
 
-          <article className='flex justify-center gap-11 m-11'>
+          <article className='flex justify-center gap-10 m-10'>
             <div>
               <h3 className='font-semibold text-lg text-center m-2 cursor-default'>Virtual</h3>
               <div>
-                <a href='https://www.facebook.com/jose.zavaleta.169405' className='flex justify-center items-center gap-6 bg-[#77C6CD]/50 rounded-md shadow-lg h-32 p-3'>
+                <a href='https://www.facebook.com/jose.zavaleta.169405' className='flex justify-center items-center gap-4 bg-[#77C6CD]/50 rounded-md shadow-lg h-32 p-3'>
                   <div className='flex flex-col items-center p-3'>
-                    <img src={clock} alt='clock' className='h-14' />
+                    <img src={clockSvg} alt='clock' className='h-12' />
                     <strong>Miércoles</strong>
                     <p>19:00 hs.</p>
                   </div>
                   <div className='flex flex-col items-center p-3'>
-                    <img src={clock} alt='clock' className='h-14' />
+                    <img src={clockSvg} alt='clock' className='h-12' />
                     <strong>Viernes</strong>
                     <p>19:00 hs.</p>
                   </div>
@@ -61,7 +57,7 @@ export function App () {
             <div>
               <h3 className='font-semibold text-lg text-center m-2 cursor-default'>Presencial</h3>
               <div className='flex flex-col items-center bg-[#77C6CD]/50 rounded-md shadow-lg h-32 p-3 cursor-default'>
-                <img src={clock} alt='clock' className='h-14' />
+                <img src={clockSvg} alt='clock' className='h-12' />
                 <strong>Domingo</strong>
                 <p>10:00 hs.</p>
               </div>
@@ -71,15 +67,15 @@ export function App () {
             📍<strong className='text-2xl hover:underline'>Mercedes 918 - CABA</strong>
           </a>
         </section>
-      </main>
+      </div>
 
       <footer id='contact' className='bg-gray-500/75 h-28 flex flex-col items-center justify-center gap-3'>
         <div className='flex justify-center gap-4'>
-          <a href='https://www.facebook.com/iglesiadedios.florestanorte'><img src={facebookLogo} alt='ícono de facebook' className='h-6' /></a>
-          <a href='https://goo.gl/maps/YZrSezH9TQe6Jyy76'><img src={locationLogo} alt='ícono de ubicación' className='h-6' /></a>
+          <a href='https://www.facebook.com/iglesiadedios.florestanorte'><img src='' alt='ícono de facebook' className='h-6' /></a>
+          <a href='https://goo.gl/maps/YZrSezH9TQe6Jyy76'><img src='' alt='ícono de ubicación' className='h-6' /></a>
         </div>
         <p className='text-center font-semibold'>©️ 2023. Proyecto de Vida todos los derechos reservados.</p>
       </footer>
-    </body>
+    </main>
   )
 }
