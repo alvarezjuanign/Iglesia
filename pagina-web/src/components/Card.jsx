53c35684f5f31f14e464ce68d97bd14b64575012
Juan Alvarez <alvarez.juanign@gmail.com>
2023-07-06 23:18:44 -0300

    Styling

--- pagina-web/src/components/Card.jsx
+++ pagina-web/src/components/Card.jsx
@@ -3,19 +3,19 @@ import clockSvg from '../../clock.svg'
 export function Home () {
   return (
     <main className='w-full bg-[#edebeb]'>
-      <div className='flex flex-col gap-24 items-center'>
-        <header className='h-1/2 flex flex-col items-center gap-4 pt-14'>
+      <div className='flex flex-col gap-1 items-center'>
+        <header className='h-screen flex flex-col items-center justify-center gap-4'>
           <picture>
             <img
               src='../../logo.webp'
               alt='logo de la iglesia'
-              className='drop-shadow-lg h-32'
+              className='h-32'
             />
           </picture>
-          <h1 className='text-center font-bold text-2xl'>Iglesia Proyecto de Vida</h1>
-          <p className='text-center mx-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa cum iste perspiciatis vitae atque nobis asperiores! Ad blanditiis itaque suscipit in consectetur vitae. Consectetur voluptatum qui quod nam, quos quas.</p>
+          <h1 className='font-bold text-2xl'>Iglesia Proyecto de Vida</h1>
+          <p className='text-center mx-[5%] [text-wrap:balance]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa cum iste perspiciatis vitae atque nobis asperiores! Ad blanditiis itaque suscipit in consectetur vitae. Consectetur voluptatum qui quod nam, quos quas.</p>
           <button
-            className='border border-[#77C6CD]/60 rounded-lg py-1 px-3 hover:bg-[#77C6CD]/60 transition-all animate-duration-1000 hover:shadow-lg hover:scale-110'
+            className='h-14 w-56 border border-[#77C6CD]/60 rounded-full py-1 px-3 absolute bottom-36 bg-[#77C6CD]/60 duration-500 hover:shadow-lg hover:scale-110 active:scale-100'
           >
             <a
               href='https://www.facebook.com/jose.zavaleta.169405'
@@ -26,16 +26,14 @@ export function Home () {
           </button>
         </header>
 
-        <section className='flex flex-col items-center my-11'>
-          <picture>
-            <img
-              src='../../encuentros.webp'
-              alt='foto de los encuentros en la iglesia'
-              className='h-60 rounded-md'
-            />
-          </picture>
+        <section className='flex flex-col items-center h-screen'>
+          <div
+            className={"h-72 w-full rounded-md bg-[url('../../encuentros.webp')] bg-cover bg-no-repeat bg-center flex justify-center items-center filter grayscale"}
+          >
+            <h2 className='text-white font-semibold text-4xl'>Encuentros</h2>
+          </div>
 
-          <article className='flex flex-col justify-center gap-10 m-10'>
+          <article className='flex flex-col justify-center gap-5 my-4'>
             <div>
               <h3 className='font-semibold text-lg text-center m-2 cursor-default'>Virtual</h3>
               <div>
@@ -70,20 +68,32 @@ export function Home () {
       </div>
 
       <section className='p-6 bg-neutral-300 flex flex-col items-center'>
-        <figure className='flex flex-col items-center'>
-          <img src='https://scontent.faep8-1.fna.fbcdn.net/v/t1.6435-9/39676124_966966570173251_8276950468190535680_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=A7XZNdXOzBwAX92_k9A&_nc_oc=AQnYyxrkMdznfSDQFH3LK_eFnsGz-B35jdopFoKA4VkMfyd1_fHf5MVbZxnxHoG5pUM&_nc_ht=scontent.faep8-1.fna&oh=00_AfA-7okkBtCFnncSjRui7937CMBasGVO2_d3P0NUXpguRA&oe=64CE9CC8' alt='' />
-          <figcaption>Encuentros en la Iglesia</figcaption>
-          <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid, quos, saepe repellendus dicta neque mollitia harum reprehenderit a tenetur sint provident rem omnis sunt natus, voluptatibus impedit nesciunt eligendi!</p>
+        <figure className='flex flex-col items-center m-5'>
+          <img
+            src='https://scontent.faep8-1.fna.fbcdn.net/v/t1.6435-9/39676124_966966570173251_8276950468190535680_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=A7XZNdXOzBwAX92_k9A&_nc_oc=AQnYyxrkMdznfSDQFH3LK_eFnsGz-B35jdopFoKA4VkMfyd1_fHf5MVbZxnxHoG5pUM&_nc_ht=scontent.faep8-1.fna&oh=00_AfA-7okkBtCFnncSjRui7937CMBasGVO2_d3P0NUXpguRA&oe=64CE9CC8'
+            alt=''
+            className='rounded-md'
+          />
+          <figcaption className='font-bold text-lg'>Nuestros jóvenes</figcaption>
+          <p className='text-center [text-wrap:balance]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid, quos, saepe repellendus dicta neque mollitia harum reprehenderit a tenetur sint provident rem omnis sunt natus, voluptatibus impedit nesciunt eligendi!</p>
         </figure>
-        <figure className='flex flex-col items-center'>
-          <img src='https://scontent.faep8-3.fna.fbcdn.net/v/t1.6435-9/89972379_1381093502093887_3928487271459717120_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UHDDPLuw3CgAX9RCYFH&_nc_ht=scontent.faep8-3.fna&oh=00_AfDDvMiDueNXzrHindKF9W65RBL_S9JfoPARFpQVYnne2w&oe=64CE9F0F' alt='' />
-          <figcaption>Encuentros en la Iglesia</figcaption>
-          <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid, quos, saepe repellendus dicta neque mollitia harum reprehenderit a tenetur sint provident rem omnis sunt natus, voluptatibus impedit nesciunt eligendi!</p>
+        <figure className='flex flex-col items-center m-5'>
+          <img
+            src='https://scontent.faep8-3.fna.fbcdn.net/v/t1.6435-9/89972379_1381093502093887_3928487271459717120_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UHDDPLuw3CgAX9RCYFH&_nc_ht=scontent.faep8-3.fna&oh=00_AfDDvMiDueNXzrHindKF9W65RBL_S9JfoPARFpQVYnne2w&oe=64CE9F0F'
+            alt=''
+            className='rounded-md'
+          />
+          <figcaption className='font-bold text-lg'>Encuentros en la Iglesia</figcaption>
+          <p className='text-center [text-wrap:balance]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid, quos, saepe repellendus dicta neque mollitia harum reprehenderit a tenetur sint provident rem omnis sunt natus, voluptatibus impedit nesciunt eligendi!</p>
         </figure>
-        <figure className='flex flex-col items-center'>
-          <img src='https://scontent.faep8-1.fna.fbcdn.net/v/t39.30808-6/340618905_234415172429762_6663431013710218391_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=QPTsm2mcMe8AX854nA2&_nc_ht=scontent.faep8-1.fna&oh=00_AfBgBdfyuECEU0bmaqqiOgF7rk7D_FL8-zMRAldtZ45G8w&oe=64AB879D' alt='' />
-          <figcaption>Encuentros en la Iglesia</figcaption>
-          <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid, quos, saepe repellendus dicta neque mollitia harum reprehenderit a tenetur sint provident rem omnis sunt natus, voluptatibus impedit nesciunt eligendi!</p>
+        <figure className='flex flex-col items-center m-5'>
+          <img
+            src='https://scontent.faep8-1.fna.fbcdn.net/v/t39.30808-6/340618905_234415172429762_6663431013710218391_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=QPTsm2mcMe8AX854nA2&_nc_ht=scontent.faep8-1.fna&oh=00_AfBgBdfyuECEU0bmaqqiOgF7rk7D_FL8-zMRAldtZ45G8w&oe=64AB879D'
+            alt=''
+            className='rounded-md'
+          />
+          <figcaption className='font-bold text-lg'>Compartiendo momentos</figcaption>
+          <p className='text-center [text-wrap:balance]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid, quos, saepe repellendus dicta neque mollitia harum reprehenderit a tenetur sint provident rem omnis sunt natus, voluptatibus impedit nesciunt eligendi!</p>
         </figure>
       </section>
 
