03c41539f8bf270ce7afc04ffef8f67edb4cac9c
Juan Alvarez <alvarez.juanign@gmail.com>
2023-07-11 19:16:36 -0300

    change: images of schedule and style

--- proyectoDeVida/src/components/Header.jsx
+++ proyectoDeVida/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import logoIglesia from '../assets/logo.webp'
-import clock from '../assets/clock.svg'
+import iglesia from '../assets/iglesia.jpg'
+import virtual from '../assets/virtual.jpg'
 
 export function Header () {
   return (
     <header className='flex flex-col items-center p-10'>
-      <div className='flex flex-col items-center justify-center gap-4 h-screen md:flex-row'>
+      <div className='flex flex-col items-center justify-center gap-5 h-screen max-w-screen-lg md:flex-row'>
         <picture>
           <img
             src={logoIglesia}
             alt='logo de la iglesia'
-            className='flex-auto max-w-xs'
+            className='max-w-xs'
           />
         </picture>
         <div className='flex flex-col items-center gap-5'>
@@ -18,7 +19,7 @@ export function Header () {
             className='h-14 w-56 border border-[#77C6CD]/60 rounded-full py-1 px-3 bg-[#77C6CD]/60 duration-500 hover:shadow-lg hover:scale-110 active:scale-100'
           >
             <a
-              href='https://www.facebook.com/jose.zavaleta.169405'
+              href='https://www.facebook.com/iglesiadedios.florestanorte'
               className='font-bold'
             >
               CONOCENOS!!
@@ -33,31 +34,24 @@ export function Header () {
 
       <section className='flex flex-col items-center'>
         <article className='flex flex-col justify-center gap-5 my-4 md:flex-row'>
-          <div>
-            <h3 className='font-semibold text-lg text-center cursor-default'>Virtual</h3>
-            <div>
-              <a href='https://www.facebook.com/jose.zavaleta.169405' className='flex justify-center items-center gap-4 bg-[#77C6CD]/50 rounded-md shadow-lg h-32'>
-                <div className='flex flex-col items-center p-3'>
-                  <img src={clock} alt='clock' className='h-12' />
-                  <strong>Miércoles</strong>
-                  <p>19:00 hs.</p>
-                </div>
-                <div className='flex flex-col items-center p-3'>
-                  <img src={clock} alt='clock' className='h-12' />
-                  <strong>Viernes</strong>
-                  <p>19:00 hs.</p>
-                </div>
-              </a>
-            </div>
+          <div class='max-w-xs bg-white border border-gray-200 rounded-lg shadow'>
+            <a href='https://www.facebook.com/jose.zavaleta.169405'>
+              <img class='rounded-t-lg' src={virtual} alt='imagen ilustrativa de encuentros virtuales' />
+              <div class='p-5'>
+                <h2 class='mb-2 text-2xl text-center'>Virtual</h2>
+                <p class='mb-3 text-center font-normal'>Miércoles y Viernes 19:00 hs.</p>
+              </div>
+            </a>
           </div>
 
-          <div>
-            <h3 className='font-semibold text-lg text-center cursor-default'>Presencial</h3>
-            <div className='flex flex-col items-center bg-[#77C6CD]/50 rounded-md shadow-lg h-32 p-3 cursor-default'>
-              <img src={clock} alt='clock' className='h-12' />
-              <strong>Domingo</strong>
-              <p>10:00 hs.</p>
-            </div>
+          <div class='max-w-xs bg-white border border-gray-200 rounded-lg shadow'>
+            <a href='https://www.facebook.com/iglesiadedios.florestanorte'>
+              <img class='rounded-t-lg' src={iglesia} alt='imagen ilustrativa de encuentros en la iglesia' />
+              <div class='p-5'>
+                <h2 class='mb-2 text-2xl text-center'>Presencial</h2>
+                <p class='mb-3 text-center font-normal'>Domingo 10:00 hs.</p>
+              </div>
+            </a>
           </div>
         </article>
       </section>
