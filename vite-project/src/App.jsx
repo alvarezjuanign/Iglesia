5c206312d76eefeb63c2fbe0f3d32c886868c2dc
Juan Alvarez <alvarez.juanign@gmail.com>
2023-07-10 21:08:35 -0300

    add: intersecting observer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,36 @@
-export function Home () {
+import { useEffect, useRef, useState } from 'react'
+
+export function App () {
+  const boxRef = useRef(null)
+  const [isIntersected, setIsIntersected] = useState(false)
+
+  const callback = (entries) => {
+    const [entry] = entries
+    setIsIntersected(entry.isIntersecting)
+  }
+
+  const options = {
+    treshold: 0.5,
+    rootMargin: '40px',
+    root: null
+  }
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(callback, options)
+    if (boxRef.current) observer.observe(boxRef.current)
+
+    return () => {
+      if (boxRef.current) observer.unobserve(boxRef.current)
+    }
+  }, [boxRef, options])
+
   return (
     <main className='w-full bg-[#edebeb]'>
       <div className='flex flex-col items-center p-10'>
         <header className='flex flex-col items-center justify-center gap-4 h-screen'>
           <picture>
             <img
-              src='../../logo.webp'
+              src='./assets/logo.webp'
               alt='logo de la iglesia'
               className='h-32'
             />
@@ -34,12 +59,12 @@ export function Home () {
               <div>
                 <a href='https://www.facebook.com/jose.zavaleta.169405' className='flex justify-center items-center gap-4 bg-[#77C6CD]/50 rounded-md shadow-lg h-32'>
                   <div className='flex flex-col items-center p-3'>
-                    <img src='../../clock.svg' alt='clock' className='h-12' />
+                    <img src='./assets/clock.svg' alt='clock' className='h-12' />
                     <strong>Miércoles</strong>
                     <p>19:00 hs.</p>
                   </div>
                   <div className='flex flex-col items-center p-3'>
-                    <img src='../../clock.svg' alt='clock' className='h-12' />
+                    <img src='./assets/clock.svg' alt='clock' className='h-12' />
                     <strong>Viernes</strong>
                     <p>19:00 hs.</p>
                   </div>
@@ -50,7 +75,7 @@ export function Home () {
             <div>
               <h3 className='font-semibold text-lg text-center cursor-default'>Presencial</h3>
               <div className='flex flex-col items-center bg-[#77C6CD]/50 rounded-md shadow-lg h-32 p-3 cursor-default'>
-                <img src='../../clock.svg' alt='clock' className='h-12' />
+                <img src='./assets/clock.svg' alt='clock' className='h-12' />
                 <strong>Domingo</strong>
                 <p>10:00 hs.</p>
               </div>
@@ -59,8 +84,8 @@ export function Home () {
         </section>
       </div>
 
-      <section className='p-6 bg-neutral-300 flex flex-col items-center'>
-        <figure className='flex flex-col items-center m-5'>
+      <section ref={boxRef} className='p-6 bg-neutral-300 flex flex-col items-center'>
+        <figure className={isIntersected ? 'animate-fade-right animate-once animate-duration-[500ms] animate-delay-500 animate-ease-in-out flex flex-col items-center m-5' : 'hidden'}>
           <img
             src='https://scontent.faep8-1.fna.fbcdn.net/v/t1.6435-9/39676124_966966570173251_8276950468190535680_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=A7XZNdXOzBwAX92_k9A&_nc_oc=AQnYyxrkMdznfSDQFH3LK_eFnsGz-B35jdopFoKA4VkMfyd1_fHf5MVbZxnxHoG5pUM&_nc_ht=scontent.faep8-1.fna&oh=00_AfA-7okkBtCFnncSjRui7937CMBasGVO2_d3P0NUXpguRA&oe=64CE9CC8'
             alt=''
@@ -69,7 +94,7 @@ export function Home () {
           <figcaption className='font-bold text-lg'>Nuestros jóvenes</figcaption>
           <p className='text-center [text-wrap:balance]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid, quos, saepe repellendus dicta neque mollitia harum reprehenderit a tenetur sint provident rem omnis sunt natus, voluptatibus impedit nesciunt eligendi!</p>
         </figure>
-        <figure className='flex flex-col items-center m-5'>
+        <figure className='animate-fade-right animate-once animate-duration-[2000ms] animate-delay-500 animate-ease-in-out flex flex-col items-center m-5'>
           <img
             src='https://scontent.faep8-3.fna.fbcdn.net/v/t1.6435-9/89972379_1381093502093887_3928487271459717120_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UHDDPLuw3CgAX9RCYFH&_nc_ht=scontent.faep8-3.fna&oh=00_AfDDvMiDueNXzrHindKF9W65RBL_S9JfoPARFpQVYnne2w&oe=64CE9F0F'
             alt=''
@@ -78,7 +103,7 @@ export function Home () {
           <figcaption className='font-bold text-lg'>Encuentros en la Iglesia</figcaption>
           <p className='text-center [text-wrap:balance]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid, quos, saepe repellendus dicta neque mollitia harum reprehenderit a tenetur sint provident rem omnis sunt natus, voluptatibus impedit nesciunt eligendi!</p>
         </figure>
-        <figure className='flex flex-col items-center m-5'>
+        <figure className='animate-fade-right animate-once animate-duration-[2000ms] animate-delay-500 animate-ease-in-out flex flex-col items-center m-5'>
           <img
             src='https://scontent.faep8-1.fna.fbcdn.net/v/t39.30808-6/340618905_234415172429762_6663431013710218391_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=QPTsm2mcMe8AX854nA2&_nc_ht=scontent.faep8-1.fna&oh=00_AfBgBdfyuECEU0bmaqqiOgF7rk7D_FL8-zMRAldtZ45G8w&oe=64AB879D'
             alt=''
