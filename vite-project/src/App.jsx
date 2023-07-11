33795cd5908f23eda5433715dfa4d6b7e5123360
Juan Alvarez <alvarez.juanign@gmail.com>
2023-07-11 15:37:15 -0300

    add: children prop to function About

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { About } from './components/about'
+import { About } from './components/About.jsx'
 
 export function App () {
   return (
@@ -63,17 +63,38 @@ export function App () {
       </div>
 
       <About
-        srcImage='https://scontent.faep8-3.fna.fbcdn.net/v/t1.6435-9/89972379_1381093502093887_3928487271459717120_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UHDDPLuw3CgAX9RCYFH&_nc_ht=scontent.faep8-3.fna&oh=00_AfDDvMiDueNXzrHindKF9W65RBL_S9JfoPARFpQVYnne2w&oe=64CE9F0F'
         animation='animate-fade-right animate-once animate-duration-[2000ms] animate-delay-200 animate-ease-in-out'
-      />
+      >
+        <img
+          src=''
+          alt=''
+          className='rounded-md'
+        />
+        <figcaption className='font-bold text-lg'>'Nuestros jóvenes'</figcaption>
+        <p className='text-center [text-wrap:balance]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, neque aspernatur cupiditate architecto quos commodi. Fuga quidem libero ad ipsam, ea ratione beatae repudiandae blanditiis perferendis non, architecto exercitationem cum!</p>
+      </About>
       <About
-        srcImage='https://scontent.faep8-1.fna.fbcdn.net/v/t39.30808-6/340618905_234415172429762_6663431013710218391_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=QPTsm2mcMe8AX854nA2&_nc_ht=scontent.faep8-1.fna&oh=00_AfBgBdfyuECEU0bmaqqiOgF7rk7D_FL8-zMRAldtZ45G8w&oe=64AB879D'
         animation='animate-fade-left animate-once animate-duration-[2000ms] animate-delay-200 animate-ease-in-out'
-      />
+      >
+        <img
+          src=''
+          alt=''
+          className='rounded-md'
+        />
+        <figcaption className='font-bold text-lg'>'Encuentros en la Iglesia'</figcaption>
+        <p className='text-center [text-wrap:balance]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, neque aspernatur cupiditate architecto quos commodi. Fuga quidem libero ad ipsam, ea ratione beatae repudiandae blanditiis perferendis non, architecto exercitationem cum!</p>
+      </About>
       <About
-        srcImage='https://scontent.faep8-1.fna.fbcdn.net/v/t1.6435-9/39676124_966966570173251_8276950468190535680_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=A7XZNdXOzBwAX92_k9A&_nc_oc=AQnYyxrkMdznfSDQFH3LK_eFnsGz-B35jdopFoKA4VkMfyd1_fHf5MVbZxnxHoG5pUM&_nc_ht=scontent.faep8-1.fna&oh=00_AfA-7okkBtCFnncSjRui7937CMBasGVO2_d3P0NUXpguRA&oe=64CE9CC8'
         animation='animate-fade-right animate-once animate-duration-[2000ms] animate-delay-200 animate-ease-in-out'
-      />
+      >
+        <img
+          src=''
+          alt=''
+          className='rounded-md'
+        />
+        <figcaption className='font-bold text-lg'>'Compartiendo momentos'</figcaption>
+        <p className='text-center [text-wrap:balance]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, neque aspernatur cupiditate architecto quos commodi. Fuga quidem libero ad ipsam, ea ratione beatae repudiandae blanditiis perferendis non, architecto exercitationem cum!</p>
+      </About>
 
       <footer id='contact' className='bg-[#77C6CD]/50 h-28 flex flex-col items-center justify-center gap-3'>
         <div className='flex justify-center gap-4'>
